679c808b2629e10c3dc31251c8ecb22991c08606
Andres Felipe Escobar Cardona <57599404+AndresEscobarDev@users.noreply.github.com>
2022-01-03 18:48:05 -0500

    0x02. Unity - Scripting Tasks: 0, 1, 2

---
+++ 0x02-unity-scripting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public Rigidbody rb;
+
+  public float speed = 1000f;
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    if (Input.GetKey("a")) rb.AddForce(-speed * Time.deltaTime, 0, 0);
+    if (Input.GetKey("w")) rb.AddForce(0, 0, speed * Time.deltaTime);
+    if (Input.GetKey("s")) rb.AddForce(0, 0, -speed * Time.deltaTime);
+    if (Input.GetKey("d")) rb.AddForce(speed * Time.deltaTime, 0, 0);
+  }
+}
